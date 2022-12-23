6aad218d29d90bbb068381edc56cb970253c9c1a
VadimTataru <64976256+VadimTataru@users.noreply.github.com>
2022-12-23 08:53:52 +0300

    feat: add button

---
+++ app/src/main/java/com/fox/bluetooth_mobile_controller/screens/MainScreen.kt
@@ -0,0 +1,19 @@
+package com.fox.bluetooth_mobile_controller.screens
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen() {
+    Scaffold{
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Поиск устройств")
+        }
+    }
+}
