2b6e2c5b1bad7eadbd94be47b913d999e31bfc6c
VadimTataru <64976256+VadimTataru@users.noreply.github.com>
2022-12-25 15:48:17 +0300

    init: start work on bluetooth

--- app/src/main/java/com/fox/bluetooth_mobile_controller/screens/MainScreen.kt
+++ app/src/main/java/com/fox/bluetooth_mobile_controller/screens/MainScreen.kt
@@ -1,15 +1,14 @@
 package com.fox.bluetooth_mobile_controller.screens
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.fox.bluetooth_mobile_controller.ui.theme.BluetoothMobileControllerTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -17,14 +16,41 @@ fun MainScreen() {
     Scaffold{
         Column(
             modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
+            verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            Text(
+                text = "Bluetooth test application \n version: 0.0.1:alpha",
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.height(60.dp))
+
             Button(
                 onClick = { /*TODO*/ },
+                modifier = Modifier.width(200.dp)
+            ) {
+                Text(text = "Включить Bluetooth")
+            }
+
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier.width(200.dp)
             ) {
                 Text(text = "Поиск устройств")
             }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    BluetoothMobileControllerTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            MainScreen()
         }
     }
 }
